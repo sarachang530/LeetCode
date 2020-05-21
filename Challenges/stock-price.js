f81515cbef3434d97750bfb8c63777445efd3016
Sara Chang <sarachang530@gmail.com>
2020-05-20 23:20:33 -0400

    stockprice problem

---
+++ Challenges/stock-price.js
@@ -0,0 +1,9 @@
+First, I wanna know how much money I could have made yesterday if I'd been trading Apple stocks all day.
+
+So I grabbed Apple's stock prices from yesterday and put them in an array called stockPrices, where:
+
+The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
+The values are the price (in US dollars) of one share of Apple stock at that time.
+So if the stock cost $500 at 10:30am, that means stockPrices[60] = 500.
+
+Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
