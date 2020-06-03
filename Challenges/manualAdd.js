8c00046b54680f251f5afa917c219c10717c7fd1
Sara Chang <sarachang530@gmail.com>
2020-06-02 22:55:16 -0400

    first attempt at manualAdd.js

--- Challenges/manualAdd.js
+++ Challenges/manualAdd.js
@@ -16,3 +16,26 @@ Input:
 firstNumber: string
 secondNumber: string
  */
+
+
+function manualAdd(firstNumber, secondNumber) {
+  let sum = '';
+  const maxLength = Math.max(firstNumber.length, secondNumber.length);
+  let carryOver = '';
+
+  for (let i = maxLength; i >= 0; i -= 1) {
+    const first = firstNumber.slice(firstNumber.length - 1);
+    const second = secondNumber.slice(secondNumber.length - 1) || 0;
+    const tempSum = Number(first) + Number(second) + carryOver;
+
+    if (tempSum > 9) {
+      carryOver = 1;
+    } else {
+      carryOver = '';
+    }
+    sum = (tempSum[tempSum.length - 1] || tempSum) + sum;    
+    firstNumber = firstNumber.substring(0, firstNumber.length - 1);
+    secondNumber = secondNumber.substring(0, secondNumber.length - 1);
+  }
+   return parseInt(sum, 10);
+ }
