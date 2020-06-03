072fc8dee4b17a050f01320cdcc23f7ba3f9e5e5
Sara Chang <sarachang530@gmail.com>
2020-06-02 22:54:36 -0400

    created manualAdd.js

---
+++ Challenges/manualAdd.js
@@ -0,0 +1,18 @@
+// add two numbers the same way you would do manually 
+/**
+ * Write an algorithm that performs an addition in a way that is done manually.
+
+Example: 
+Adding two numbers given as a string: 253 & 29
+
+    253
+  +  29
+    (1)
+  ------
+    282
+
+
+Input: 
+firstNumber: string
+secondNumber: string
+ */
