7d83920bb098dee36cdc78c452b27ec027c623d9
Sara Chang <sarachang530@gmail.com>
2020-06-06 20:16:22 -0400

    oddevenLLsolved

--- Challenges/oddEvenLL.js
+++ Challenges/oddEvenLL.js
@@ -37,6 +37,22 @@ Submissions
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
-    
+var oddEvenList = function (head) {
+  if (head === null) {
+    return null;
+  }
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+
+  while (even !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
 };
