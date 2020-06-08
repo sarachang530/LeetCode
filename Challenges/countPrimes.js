f4aa88f25f5b62337daef7774cc8c27ba5b79165
Sara Chang <sarachang530@gmail.com>
2020-06-08 12:58:23 -0400

    countprimes solved

--- Challenges/countPrimes.js
+++ Challenges/countPrimes.js
@@ -12,6 +12,27 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
  * @param {number} n
  * @return {number}
  */
-var countPrimes = function(n) {
-    
-};
+let countPrimes = function(n) {
+  let primes = [];
+  let primeCount = 0;
+
+  for (let i = 0; i < n; i++) {
+    primes.push(true);
+  }
+
+  for (let i = 2; i * i < primes.length; i++) {
+    if (primes[i]) {
+      for (let j = i; j * i < primes.length; j++) {
+        primes[i * j] = false;
+      }
+    }
+  }
+
+  for (let i = 2; i < primes.length; i++) {
+    if (primes[i]) {
+      primeCount++;
+    }
+  }
+
+  return primeCount;
+}
