ac491329ad2246764e98dc6eeb50703c04fd1960
Sara Chang <sarachang530@gmail.com>
2020-07-09 17:41:17 -0400

    shuffle the array.js completed

--- Challenges/shuffleTheArray.js
+++ Challenges/shuffleTheArray.js
@@ -30,3 +30,22 @@ nums.length == 2n
 1 <= nums[i] <= 10^3
 
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+
+
+
+var shuffle = function(nums, n) {
+  let result = []
+  for(let i = 0; i< nums.length/2; i++){
+    result.push(nums[i])
+    result.push(nums[n+i])
+    }
+    return result;
+};
+
+console.log(shuffle([2,5,1,3,4,7],3))
