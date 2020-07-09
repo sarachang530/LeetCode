fb06ec4f379adc6a6f86200dbe631e04d28d979e
Sara Chang <sarachang530@gmail.com>
2020-07-09 16:47:44 -0400

    running sum of 1d array.js completed

--- Challenges/runningSumof1dArr.js
+++ Challenges/runningSumof1dArr.js
@@ -32,4 +32,19 @@ Output: [3,4,6,16,17]
  */
 
 
- 
+ /**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+  let result = [];
+  let sum = 0;
+  for(let i = 0; i < nums.length; i++){
+    sum += nums[i]
+    result.push(sum);
+  }
+  return result;
+};
+
+console.log(runningSum([1,2,3,4]))
+
