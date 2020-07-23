cd0528ba5433bad092466d0bd267620aba1a02bd
Sara Chang <sarachang530@gmail.com>
2020-07-22 21:11:27 -0400

    included leetcode url link

--- Challenges/kidsWithTheGreatestNumOfCandies.js
+++ Challenges/kidsWithTheGreatestNumOfCandies.js
@@ -1,15 +1,8 @@
 /*
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
 
 1431. Kids With the Greatest Number of Candies
-Easy
-
-267
-
-71
-
-Add to List
-
-Share
 Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
 
 For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
